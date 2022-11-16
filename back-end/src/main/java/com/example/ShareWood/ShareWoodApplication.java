3cff946502c7aacaefdc0877a9f3666c9a2c104e
lucas-987 <69002344+lucas-987@users.noreply.github.com>
2022-11-16 11:37:31 +0100

    first commit

---
+++ back-end/src/main/java/com/example/ShareWood/ShareWoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShareWood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareWoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareWoodApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/example/ShareWood/ShareWoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShareWood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareWoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
