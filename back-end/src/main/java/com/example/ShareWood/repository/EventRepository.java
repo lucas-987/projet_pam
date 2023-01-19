7e0c2c56518c7e0ba77101cc90bf2df928eb6cfd
Robin Marlin-Lefebvre <robin.marlin-lefebvre@edu.univ-fcomte.fr>
2023-01-19 16:20:09 +0100

    Ajout gestion évènements en partie

--- back-end/src/main/java/com/example/ShareWood/repository/EventRepository.java
+++ back-end/src/main/java/com/example/ShareWood/repository/EventRepository.java
@@ -4,6 +4,10 @@ import com.example.ShareWood.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByLocation(String location);
+    List<Event> findByCreator(int id);
 }
--- back-end/src/main/java/com/example/ShareWood/service/IEventService.java
+++ back-end/src/main/java/com/example/ShareWood/service/IEventService.java
@@ -2,13 +2,15 @@ package com.example.ShareWood.service;
 
 import com.example.ShareWood.entities.Event;
 
+import java.util.Date;
 import java.util.List;
 
 public interface IEventService {
     public List<Event> getAllEventByLocation(String location);
+    public List<Event> getAllEventByUser(int user);
     public List<Event> getAllEvent();
     public Event getEventById(Long id);
-    public Event addEvent(Event event);
+    public Event addEvent(String name, String description, Date date_debut, Date date_fin, String location);
     public Event updateEvent(Long id,Event event);
     public void deleteEvent(Long id);
     public void addParticipantToEvent(Long idUser, Long idEvent);
